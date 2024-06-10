703c076708b2eeab19d2780e363b1f6668029ea6
Maxence C. <maxchaix54@gmail.com>
2024-06-10 11:06:49 +0200

    initialisation carte sur Nancy

---
+++ js/map.js
@@ -0,0 +1,6 @@
+const map = L.map('map').setView([48.6893, 6.1790], 13);
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
