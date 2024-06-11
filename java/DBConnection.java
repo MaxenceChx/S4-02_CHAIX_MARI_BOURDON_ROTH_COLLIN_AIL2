4f8360bd36312b0e4abb2cb53d3f7d53e425cb59
Marin <bourdonmarin@gmail.com>
2024-06-11 11:22:54 +0200

    ajout d'insert de restaurants en dur pour test

--- java/DBConnection.java
+++ java/DBConnection.java
@@ -15,18 +15,31 @@ public class DBConnection {
             System.out.println("connecté");
             return var0;
         } catch (ClassNotFoundException var1) {
-            throw new ClassNotFoundException("Oracle JDBC driver not found.");
+            throw new ClassNotFoundException("Oracle JDBC driver non trouvé.");
         }
     }
 
     public static void main(String[] var0) throws ClassNotFoundException, SQLException, InterruptedException {
-        Connection var1 = etablirConnexion();
-        String var2 = "SELECT nom from restaurant";
-        ResultSet var3 = var1.createStatement().executeQuery(var2);
-
-        while(var3.next()) {
-            System.out.println(var3.getString(1));
-        }
-
+        Connection connection = etablirConnexion();
+        String query = "INSERT INTO restaurant(num, nom, adresse, latitude, longitude) VALUES(1, 'Brasserie L excelsior', '50 Rue Henri-Poincaré, 54000 Nancy', '48.690775', '6.175630')";
+        connection.createStatement().executeUpdate(query);
+        query = "INSERT INTO restaurant(num, nom, adresse, latitude, longitude) VALUES(2, 'Piopa Lasagnax', '61 Rue Pierre Semard, 54000 Nancy', '48.689304', '6.178177')";
+        connection.createStatement().executeUpdate(query);
+        query = "INSERT INTO restaurant(num, nom, adresse, latitude, longitude) VALUES(3, 'Le Café Lecce', '3 Rue Crampel, 54000 Nancy', '48.689555', '6.175335')";
+        connection.createStatement().executeUpdate(query);
+        query = "INSERT INTO restaurant(num, nom, adresse, latitude, longitude) VALUES(4, 'Piperno', '1 Bis Rue Notre Dame, 54000 Nancy', '48.689555', '6.180362')";
+        connection.createStatement().executeUpdate(query);
+        query = "INSERT INTO restaurant(num, nom, adresse, latitude, longitude) VALUES(5, 'Coté Sushi', '18 Pl. Henri Mengin, 54000 Nancy', '48.689658', '6.182100')";
+        connection.createStatement().executeUpdate(query);
+        query = "INSERT INTO restaurant(num, nom, adresse, latitude, longitude) VALUES(6, 'Tanto Bene', '1 Av. Foch, 54000 Nancy', '48.689502', '6.177079')";
+        connection.createStatement().executeUpdate(query);
+        query = "INSERT INTO restaurant(num, nom, adresse, latitude, longitude) VALUES(7, 'Le Dizneuf', '19 Pl. Henri Mengin, 54000 Nancy', '48.688765', '6.182293')";
+        connection.createStatement().executeUpdate(query);
+        query = "INSERT INTO restaurant(num, nom, adresse, latitude, longitude) VALUES(8, 'Brasserie Simone', '5 place Simone Veil, 54000 Nancy', '48.689678', '6.175585')";
+        connection.createStatement().executeUpdate(query);
+        query = "INSERT INTO restaurant(num, nom, adresse, latitude, longitude) VALUES(9, 'Jean Lamour', '7-9 Pl. Stanislas, 54000 Nancy', '48.693892', '6.182529')";
+        connection.createStatement().executeUpdate(query);
+        query = "INSERT INTO restaurant(num, nom, adresse, latitude, longitude) VALUES(10, 'Au Grand Sérieux', '27 Rue Raugraff, 54000 Nancy', '48.689728', '6.181992')";
+        connection.createStatement().executeUpdate(query);
     }
 }
